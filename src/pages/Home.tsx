f581ec16a7cc56cbf9c59636146016eba285b2ad
Isaque de Sousa <isaque.error404@gmail.com>
2023-10-29 18:14:48 -0300

    feat: create new pages

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import { Header } from "../components/Header";
+
+export function Home() {
+  return (
+    <div className="max-w-6xl h-[calc(100vh_-_10rem)] my-20 mx-auto bg-zinc-800 rounded-md">
+      <>
+        <Header />
+      </>
+    </div>
+  );
+}
