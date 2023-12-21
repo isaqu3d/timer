73b22ac62153274ce07a877821fc7bb0faf22670
Isaque de Sousa <isaque.error404@gmail.com>
2023-12-21 02:20:17 -0300

    feat: add new styles

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -1,3 +1,119 @@
+import { ReactNode } from "react";
+import { Header } from "../components/Header";
+
+const STATUS_COLORS = {
+  red: "bg-[#F03847]",
+  green: "bg-[#00B37E]",
+  yellow: "bg-[#FBA94C]",
+};
+interface StatusColorsProps {
+  statusColor: keyof typeof STATUS_COLORS;
+  children: ReactNode;
+}
+
+const Status = ({ statusColor, children }: StatusColorsProps) => {
+  const colorClass = STATUS_COLORS[statusColor];
+
+  return (
+    <span
+      className={`flex items-center gap-4 rounded-full w-2 h-2 before:content-[''] ${colorClass} whitespace-nowrap`}
+    >
+      {children}
+    </span>
+  );
+};
+
 export function History() {
-  return <div>History</div>;
+  return (
+    <div className="max-w-7xl h-[calc(100vh_-_10rem)] my-20 mx-auto bg-zinc-800 rounded-md">
+      <Header />
+      <div className="historyContainer flex-1 p-14 flex flex-col">
+        <h1 className="text-2xl font-bold text-gray-100">Meu histórico</h1>
+
+        <div className="historyList flex-1 overflow-auto mt-8">
+          <table className="w-full border-collapse min-w-[600px]">
+            <thead>
+              <tr>
+                <th className="bg-gray-600 p-4 text-left text-gray-100 text-sm leading-4 rounded-t-lg">
+                  Tarefa
+                </th>
+                <th className="bg-gray-600 p-4 text-left text-gray-100 text-sm leading-4">
+                  Duração
+                </th>
+                <th className="bg-gray-600 p-4 text-left text-gray-100 text-sm leading-4">
+                  Início
+                </th>
+
+                <th className="bg-gray-600 p-4 text-left text-gray-100 text-sm leading-4 rounded-t-lg pr-6">
+                  Status
+                </th>
+              </tr>
+            </thead>
+
+            <tbody>
+              <tr className="[&>*:first-child]:w-2/4">
+                <td className="bg-gray-500 border-t-gray-800 border-t leading-4 p-4 text-sm ">
+                  Ignite time
+                </td>
+                <td className="bg-gray-500 border-t-gray-800 border-t leading-4 p-4 text-sm">
+                  25 minutos
+                </td>
+                <td className="bg-gray-500 border-t-gray-800 border-t leading-4 p-4 text-sm">
+                  Há cerca de 2 meses
+                </td>
+                <td className="bg-gray-500 border-t-gray-800 border-t leading-4 p-4 text-sm">
+                  <Status statusColor="red">Interrompido</Status>
+                </td>
+              </tr>
+
+              <tr>
+                <td className="bg-gray-500 border-t-gray-800 border-t leading-4 p-4 text-sm">
+                  Tarefa
+                </td>
+                <td className="bg-gray-500 border-t-gray-800 border-t leading-4 p-4 text-sm">
+                  25 minutos
+                </td>
+                <td className="bg-gray-500 border-t-gray-800 border-t leading-4 p-4 text-sm">
+                  Há cerca de 2 meses
+                </td>
+                <td className="bg-gray-500 border-t-gray-800 border-t leading-4 p-4 text-sm">
+                  <Status statusColor="green">Concluído</Status>
+                </td>
+              </tr>
+
+              <tr>
+                <td className="bg-gray-500 border-t-gray-800 border-t leading-4 p-4 text-sm">
+                  Tarefa
+                </td>
+                <td className="bg-gray-500 border-t-gray-800 border-t leading-4 p-4 text-sm">
+                  25 minutos
+                </td>
+                <td className="bg-gray-500 border-t-gray-800 border-t leading-4 p-4 text-sm">
+                  Há cerca de 2 meses
+                </td>
+                <td className="bg-gray-500 border-t-gray-800 border-t leading-4 p-4 text-sm">
+                  <Status statusColor="red">Interrompido</Status>
+                </td>
+              </tr>
+
+              <tr>
+                <td className="bg-gray-500 border-t-gray-800 border-t leading-4 p-4 text-sm">
+                  Tarefa
+                </td>
+                <td className="bg-gray-500 border-t-gray-800 border-t leading-4 p-4 text-sm">
+                  25 minutos
+                </td>
+                <td className="bg-gray-500 border-t-gray-800 border-t leading-4 p-4 text-sm">
+                  Há cerca de 2 meses
+                </td>
+                <td className="bg-gray-500 border-t-gray-800 border-t  p-4 text-sm">
+                  <Status statusColor="yellow">Em andamento</Status>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { Play } from "@phosphor-icons/react";
 import { Header } from "../components/Header";
 
 export function Home() {
@@ -5,6 +6,55 @@ export function Home() {
     <div className="max-w-6xl h-[calc(100vh_-_10rem)] my-20 mx-auto bg-zinc-800 rounded-md">
       <>
         <Header />
+
+        <div className="homecontainer flex-1 flex flex-col items-center justify-center">
+          <form action="" className="flex flex-col items-center gap-14">
+            <div className="formcontainer w-full flex flex-wrap items-center justify-center gap-2 font-bold text-lg">
+              <label htmlFor="task">Vou trabalhar em</label>
+              <input
+                type="text"
+                id="task"
+                className="bg-transparent border-b border-b-zinc-500 outline-none h-10 font-bold p-2 text-gray-100 flex-1 placeholder:text-gray-400 focus:border-green-100"
+                placeholder="Dê um nome para seu projeto"
+                list="task-suggestions"
+              />
+
+              <datalist id="task-suggestions">
+                <option value="Projeto 1" />
+                <option value="Projeto 2" />
+                <option value="Projeto 3" />
+                <option value="ignite time" />
+              </datalist>
+
+              <label htmlFor="duration">durante</label>
+              <input
+                type="number"
+                id="duration"
+                className="bg-transparent border-b border-b-zinc-500 outline-none h-10 font-bold p-2 text-gray-100 w-16  placeholder:text-gray-400 focus:border-green-100"
+                placeholder="00"
+              />
+              <span>minutos.</span>
+            </div>
+
+            <div className="countdowm text-9xl leading-[8rem] text-gray-100 flex gap-4">
+              <span className="py-8 px-4 rounded-lg bg-gray-600">0</span>
+              <span className="py-8 px-4 rounded-lg bg-gray-600">0</span>
+              <span className="py-8 text-green-500 w-16 overflow-hidden flex justify-center">
+                :
+              </span>
+              <span className="py-8 px-4 rounded-lg bg-gray-600">0</span>
+              <span className="py-8 px-4 rounded-lg bg-gray-600">0</span>
+            </div>
+
+            <button
+              disabled
+              type="submit"
+              className="w-full p-4 rounded-md font-bold gap-2 bg-green-500 text-gray-100 enabled:hover:bg-green-600 transition-colors flex justify-center items-center text-2xl disabled:opacity-70 disabled:cursor-not-allowed disabled:hover"
+            >
+              <Play /> Começar
+            </button>
+          </form>
+        </div>
       </>
     </div>
   );
