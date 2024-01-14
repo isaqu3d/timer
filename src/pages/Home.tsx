a34c5e3a6b9ea5fb042766d4d3a4b274e412bdde
Isaque de Sousa <isaque.error404@gmail.com>
2024-01-14 19:31:36 -0300

    feat: add new components and create context

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,34 +1,48 @@
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Play } from "@phosphor-icons/react";
-import { differenceInSeconds } from "date-fns";
-import { useEffect, useState } from "react";
-import { useForm } from "react-hook-form";
+import { HandPalm, Play } from "@phosphor-icons/react";
+import { createContext, useState } from "react";
+import { FormProvider, useForm } from "react-hook-form";
 import zod from "zod";
+import { Countdown } from "../components/Countdown";
+import { FormNewCycle } from "../components/FormNewCycle";
 import { Header } from "../components/Header";
-
-const newCycleFormValidationSchema = zod.object({
-  task: zod.string().min(3, "Informe a tarefa"),
-  minutesAmount: zod
-    .number()
-    .min(5, "O ciclo precisa ser de no máximo 5 minutos")
-    .max(60, "O ciclo precisa ser de no máximo 60 minutos"),
-});
-
-type newCycleFormData = zod.infer<typeof newCycleFormValidationSchema>;
 
 interface Cycle {
   id: string;
   task: string;
   minutesAmount: number;
   startDate: Date;
+  interruptedDate?: Date;
+  finishedDate?: Date;
 }
+
+interface CyclesContextType {
+  activeCycle: Cycle | undefined;
+  activeCycleId: string | null;
+  amountSecondsPassed: number;
+  markCurrentCycleAsFinished: () => void;
+  setSecondsPassed: (seconds: number) => void;
+}
+
+export const CyclesContext = createContext({} as CyclesContextType);
 
 export function Home() {
   const [cycles, setCycles] = useState<Cycle[]>([]);
   const [activeCycleId, setActiveCycleId] = useState<string | null>(null);
   const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
 
-  const { register, handleSubmit, watch, reset } = useForm<newCycleFormData>({
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
+
+  const newCycleFormValidationSchema = zod.object({
+    task: zod.string().min(3, "Informe a tarefa"),
+    minutesAmount: zod
+      .number()
+      .min(5, "O ciclo precisa ser de no máximo 5 minutos")
+      .max(60, "O ciclo precisa ser de no máximo 60 minutos"),
+  });
+
+  type newCycleFormData = zod.infer<typeof newCycleFormValidationSchema>;
+  const newCycleForm = useForm<newCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
     defaultValues: {
       task: "",
@@ -36,22 +50,23 @@ export function Home() {
     },
   });
 
-  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
+  const { handleSubmit, watch, reset } = newCycleForm;
 
-  useEffect(() => {
-    let interval: number;
-    if (activeCycle) {
-      interval = setInterval(() => {
-        setAmountSecondsPassed(
-          differenceInSeconds(new Date(), activeCycle.startDate)
-        );
-      }, 1000);
-    }
+  const setSecondsPassed = (seconds: number) => {
+    setAmountSecondsPassed(seconds);
+  };
 
-    return () => {
-      clearInterval(interval);
-    };
-  }, [activeCycle]);
+  const markCurrentCycleAsFinished = () => {
+    setCycles((state) =>
+      state.map((cycle) => {
+        if (cycle.id === activeCycleId) {
+          return { ...cycle, finishedDate: new Date() };
+        } else {
+          return cycle;
+        }
+      })
+    );
+  };
 
   const handleCreateNewCycle = (data: newCycleFormData) => {
     const id = String(new Date().getTime());
@@ -69,20 +84,19 @@ export function Home() {
     reset();
   };
 
-  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
-  const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0;
+  const handleInterruptCycle = () => {
+    setCycles((state) =>
+      state.map((cycle) => {
+        if (cycle.id === activeCycleId) {
+          return { ...cycle, interruptedDate: new Date() };
+        } else {
+          return cycle;
+        }
+      })
+    );
 
-  const minutesAmount = Math.floor(currentSeconds / 60);
-  const secondsAmount = currentSeconds % 60;
-
-  const minutes = String(minutesAmount).padStart(2, "0");
-  const seconds = String(secondsAmount).padStart(2, "0");
-
-  useEffect(() => {
-    if (activeCycle) {
-      document.title = `${minutes}: ${seconds}`;
-    }
-  }, [minutes, seconds, activeCycle]);
+    setActiveCycleId(null);
+  };
 
   const task = watch("task");
   const minutesFilled = watch("minutesAmount");
@@ -94,69 +108,44 @@ export function Home() {
       <>
         <Header />
 
-        <div className="homecontainer flex-1 flex flex-col items-center justify-center">
+        <div className="flex-1 flex flex-col items-center justify-center">
           <form
             onSubmit={handleSubmit(handleCreateNewCycle)}
             action=""
             className="flex flex-col items-center gap-14"
           >
-            <div className="formcontainer w-full flex flex-wrap items-center justify-center gap-2 font-bold text-lg">
-              <label htmlFor="task">Vou trabalhar em</label>
-              <input
-                type="text"
-                id="task"
-                className="bg-transparent border-b border-b-zinc-500 outline-none h-10 font-bold p-2 text-gray-100 flex-1 placeholder:text-gray-400 focus:border-green-100"
-                placeholder="Dê um nome para seu projeto"
-                list="task-suggestions"
-                {...register("task")}
-              />
-
-              <datalist id="task-suggestions">
-                <option value="Projeto 1" />
-                <option value="Projeto 2" />
-                <option value="Projeto 3" />
-                <option value="ignite time" />
-              </datalist>
-
-              <label htmlFor="minutesAmount">durante</label>
-              <input
-                type="number"
-                id="minutesAmount"
-                className="bg-transparent border-b border-b-zinc-500 outline-none h-10 font-bold p-2 text-gray-100 w-16  placeholder:text-gray-400 focus:border-green-100"
-                placeholder="00"
-                {...register("minutesAmount", { valueAsNumber: true })}
-              />
-              <span>minutos.</span>
-            </div>
-
-            <div className="countdowm text-9xl leading-[8rem] text-gray-100 flex gap-4">
-              <span className="py-8 px-4 rounded-lg bg-gray-600">
-                {minutes[0]}
-              </span>
-              <span className="py-8 px-4 rounded-lg bg-gray-600">
-                {" "}
-                {minutes[1]}
-              </span>
-              <span className="py-8 text-green-500 w-16 overflow-hidden flex justify-center">
-                :
-              </span>
-              <span className="py-8 px-4 rounded-lg bg-gray-600">
-                {" "}
-                {seconds[0]}
-              </span>
-              <span className="py-8 px-4 rounded-lg bg-gray-600">
-                {" "}
-                {seconds[1]}
-              </span>
-            </div>
-
-            <button
-              type="submit"
-              disabled={isSubmitDisabled}
-              className="w-full p-4 rounded-md font-bold gap-2 bg-green-500 text-gray-100 enabled:hover:bg-green-600 transition-colors flex justify-center items-center text-2xl disabled:opacity-70 disabled:cursor-not-allowed disabled:hover"
+            <CyclesContext.Provider
+              value={{
+                activeCycle,
+                activeCycleId,
+                markCurrentCycleAsFinished,
+                amountSecondsPassed,
+                setSecondsPassed,
+              }}
             >
-              <Play /> Começar
-            </button>
+              <FormProvider {...newCycleForm}>
+                <FormNewCycle />
+              </FormProvider>
+              <Countdown />
+            </CyclesContext.Provider>
+
+            {activeCycle ? (
+              <button
+                type="button"
+                onClick={handleInterruptCycle}
+                className="w-full p-4 rounded-md font-bold gap-2 bg-red-500 text-gray-100 enabled:hover:bg-red-600 transition-colors flex justify-center items-center text-2xl disabled:opacity-70 disabled:cursor-not-allowed disabled:hover"
+              >
+                <HandPalm /> Interromper
+              </button>
+            ) : (
+              <button
+                type="submit"
+                disabled={isSubmitDisabled}
+                className="w-full p-4 rounded-md font-bold gap-2 bg-green-500 text-gray-100 enabled:hover:bg-green-600 transition-colors flex justify-center items-center text-2xl disabled:opacity-70 disabled:cursor-not-allowed disabled:hover"
+              >
+                <Play /> Começar
+              </button>
+            )}
           </form>
         </div>
       </>
